02166f118b69aa6ee5c6a566ace7956036dfa6bb
NikolaCirisanovic <cirisanovic.nikola@outlook.de>
2019-06-18 17:15:42 +0700

    linking the shop to the landing page

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// Importing express to the index.js file
 const path = require('path')
 const express = require("express");
 
 const app = express();
 
-// Parameters path '/' and the function
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+// app.get("/", (req, res) => {
+//   res.send();
+// });
 
-// Static files
 
 app.use(express.static(path.join(__dirname, 'client')))
 
-app.get("/shop", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'client/shop.html')) 
 })
   
-// Listening for requests on port 3000  ; Running the server
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
